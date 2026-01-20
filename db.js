648089f3d7e8b5d2a9a9cd0b64bc1e34cfbd4013
alinesteele21-ctrl <alinesteele21@gmail.com>
2026-01-20 01:00:07 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
-// db.js - conexão PostgreSQL via Railway
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // necessário para Railway
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
+
